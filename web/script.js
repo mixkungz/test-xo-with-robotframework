c7f9b4f9293e8d76a977aaf5a131220dd41eae37
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-08-04 13:40:09 +0700

    :sparkles: Can send position to backend.

--- web/script.js
+++ web/script.js
@@ -1,11 +1,45 @@
 $(document).ready(function() {
-  $('#R1_C1').click(function() {})
-  $('#R1_C2').click(function() {})
-  $('#R1_C3').click(function() {})
-  $('#R2_C1').click(function() {})
-  $('#R2_C2').click(function() {})
-  $('#R2_C3').click(function() {})
-  $('#R3_C1').click(function() {})
-  $('#R3_C2').click(function() {})
-  $('#R3_C3').click(function() {})
+  $('#R1_C1').click(function() {
+    submitForm(this.id)
+  })
+  $('#R1_C2').click(function() {
+    submitForm(this.id)
+  })
+  $('#R1_C3').click(function() {
+    submitForm(this.id)
+  })
+  $('#R2_C1').click(function() {
+    submitForm(this.id)
+  })
+  $('#R2_C2').click(function() {
+    submitForm(this.id)
+  })
+  $('#R2_C3').click(function() {
+    submitForm(this.id)
+  })
+  $('#R3_C1').click(function() {
+    submitForm(this.id)
+  })
+  $('#R3_C2').click(function() {
+    submitForm(this.id)
+  })
+  $('#R3_C3').click(function() {
+    submitForm(this.id)
+  })
+
+  // let submitForm = position => {
+  // $('#sendPosition').val(position)
+  // console.log($('#sendPosition'))
+  // console.log($('#game-board'))
+  // console.log($('#game-board')[0])
+  // $('#game-board').submit(function(event) {
+  //   console.log('yo', event)
+  //   })
+  //   console.log('after')
+  // }
 })
+
+submitForm = function(position) {
+  $('#sendPosition').val(position)
+  document.getElementById('gameForm').submit()
+}
